221b80f5ea0bdaa85dfd5bc4f0deb5a1e257aadc
AndersonFlorencioConforto <afcaconforto@gmail.com>
2023-06-28 17:01:43 -0300

    criando Âprojeto

---
+++ PraticandoDDD/src/main/java/dev/anderson/praticandoddd/PraticandoDddApplication.java
@@ -0,0 +1,13 @@
+package dev.anderson.praticandoddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraticandoDddApplication.class, args);
+    }
+
+}
---
+++ PraticandoDDD/src/test/java/dev/anderson/praticandoddd/PraticandoDddApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anderson.praticandoddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
