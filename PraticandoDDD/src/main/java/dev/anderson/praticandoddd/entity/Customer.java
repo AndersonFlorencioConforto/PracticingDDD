74c7eed73da31e0a2ebeeb610956f08e378ce109
AndersonFlorencioConforto <afcaconforto@gmail.com>
2023-06-28 17:54:29 -0300

    adds name and id validation, and implements self-validation principle

--- PraticandoDDD/src/main/java/dev/anderson/praticandoddd/entity/Customer.java
+++ PraticandoDDD/src/main/java/dev/anderson/praticandoddd/entity/Customer.java
@@ -5,21 +5,25 @@ public class Customer {
     private String id;
     private String name;
     private String address;
-    private boolean active;
+    private boolean active = false;
 
-    public Customer(String id, String name, String address) {
+    public Customer(String id, String name) {
         this.id = id;
         this.name = name;
-        this.address = address;
+        this.validate();
     }
 
     //regra de negócio
     public void changeName(String name){
         this.name = name;
+        this.validate();
     }
 
     //regra de negócio
     public void activate(){
+        if (this.address == null || this.address.trim().equals("")){
+            throw new IllegalArgumentException("Endereço do cliente é obrigatório");
+        }
         this.active = true;
     }
 
@@ -28,8 +32,14 @@ public class Customer {
         this.active = false;
     }
 
-
-
+    public void validate(){
+        if(this.name == null || this.name.trim().equals("")){
+            throw new IllegalArgumentException("Nome do cliente é obrigatório");
+        }
+        if (this.id == null || this.id.trim().equals("")){
+            throw new IllegalArgumentException("Id do cliente é obrigatório");
+        }
+    }
 
 
 }
