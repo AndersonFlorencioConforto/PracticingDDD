320f77162f4bc8809323714dabe00ac8320cf007
AndersonFlorencioConforto <afcaconforto@gmail.com>
2023-07-03 16:21:12 -0300

    implements TDD rewards policies

--- PraticandoDDD/src/test/java/dev/anderson/praticandoddd/service/OrderServiceTest.java
+++ PraticandoDDD/src/test/java/dev/anderson/praticandoddd/service/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package dev.anderson.praticandoddd.service;
 
+import dev.anderson.praticandoddd.entity.Customer;
 import dev.anderson.praticandoddd.entity.Order;
 import dev.anderson.praticandoddd.entity.OrderItem;
 import org.junit.jupiter.api.Assertions;
@@ -23,5 +24,16 @@ public class OrderServiceTest {
         var total = OrderService.total(orderArray);
         Assertions.assertEquals(500.0, total);
 
+    }
+
+    @Test
+    void givenAValidCommandWhenCreateOrderShouldAddRewardsCustomer() {
+
+        var customer = new Customer("123", "Anderson");
+        var item1 = new OrderItem("123", "item 1", 100.00, "123", 1);
+        var order = OrderService.placeOrder(customer, List.of(item1));
+
+        Assertions.assertEquals(50, customer.getRewards());
+        Assertions.assertEquals(500.0, order.total());
     }
 }
